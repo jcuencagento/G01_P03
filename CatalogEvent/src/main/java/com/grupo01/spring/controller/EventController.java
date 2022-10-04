6eb19f0b1db2b5b55aaf7ea5e18173443803e260
Javier Cuenca Gento <100384012@alumnos.uc3m.es>
2022-10-04 17:47:29 +0200

    Commit addEvento()

--- CatalogEvent/src/main/java/com/grupo01/spring/controller/EventController.java
+++ CatalogEvent/src/main/java/com/grupo01/spring/controller/EventController.java
@@ -6,6 +6,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,11 +25,20 @@ public class EventController {
 	EventService service;
 
 	
-	@GetMapping("/")
+	@GetMapping("")
 	public List<EventoDTO> eventoListado() {
 		log.info("----Listado en EventController----");
 		final List<Evento> all = service.eventoListado();
 		return EventoDTO.of(all);
+	}
+	
+	
+	
+	
+	@PostMapping("/add")
+	public EventoDTO addEvento(@RequestBody Evento evento) {
+		log.info("----Add Evento en EventController----");
+		return EventoDTO.of(service.addEvento(evento));
 	}
 
 }
