108a50932a30b8a8d73b3cb7f50eb13f0c400ecc
Javier Cuenca Gento <100384012@alumnos.uc3m.es>
2022-10-04 18:09:09 +0200

    Commit

--- CatalogEvent/src/main/java/com/grupo01/spring/controller/EventController.java
+++ CatalogEvent/src/main/java/com/grupo01/spring/controller/EventController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,7 +33,11 @@ public class EventController {
 		return EventoDTO.of(all);
 	}
 	
-	
+	@GetMapping("/{event_id}")
+	public EventoDTO eventoByEvent_id(@PathVariable int event_id) {
+		log.info("----Listado por id de evento en EventController----");
+		return EventoDTO.of(service.eventoByEvent_id(event_id).orElseThrow());
+	}
 	
 	
 	@PostMapping("/add")
