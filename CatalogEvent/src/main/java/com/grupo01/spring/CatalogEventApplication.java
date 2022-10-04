42413b6fd10cc71e7810b5b05d32b7f2d9ede721
Javier Cuenca Gento <100384012@alumnos.uc3m.es>
2022-10-04 11:37:27 +0200

    Primer Commit.

---
+++ CatalogEvent/src/main/java/com/grupo01/spring/CatalogEventApplication.java
@@ -0,0 +1,13 @@
+package com.grupo01.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogEventApplication.class, args);
+	}
+
+}
