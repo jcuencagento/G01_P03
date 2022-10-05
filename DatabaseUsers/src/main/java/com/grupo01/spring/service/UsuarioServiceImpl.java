a029faf2e6174b8d150dd71f2186edada086ecfa
antonofv <yjabardo@gmail.com>
2022-10-05 17:05:59 +0200

    Commit openApi
    En microservicio User

--- DatabaseUsers/src/main/java/com/grupo01/spring/service/UsuarioServiceImpl.java
+++ DatabaseUsers/src/main/java/com/grupo01/spring/service/UsuarioServiceImpl.java
@@ -11,6 +11,9 @@ import com.grupo01.spring.controller.error.UserFoundException;
 import com.grupo01.spring.model.Usuario;
 import com.grupo01.spring.repository.UsuarioRepository;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+
 @Transactional
 @Service
 public class UsuarioServiceImpl implements UsuarioService {
@@ -18,7 +21,9 @@ public class UsuarioServiceImpl implements UsuarioService {
 	@Autowired
 	private UsuarioRepository usuarioRepo;
 	
-	public Usuario crearUsuario(Usuario usuario) throws Exception {
+	@Operation(summary= "Crea y añade un usuario nuevo. lo añade en la base de datos mediante la llamada al metodo save().")
+	public Usuario crearUsuario(@Parameter(description= "Usuario a añadir. Recibido desde el control.") 
+								Usuario usuario) throws Exception {
 		
 		//Optional<Usuario> duplicado= Optional.ofNullable(usuarioRepo.findByMail(usuario.getMail()).orElse(null));
 		//Usuario dupe= duplicado.get();
