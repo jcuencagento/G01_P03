e9e91388c3372543831a3883d7fe53d4e55dfe73
Javier Cuenca Gento <100384012@alumnos.uc3m.es>
2022-10-07 15:03:09 +0200

    Commit buscando arreglar Ticket

--- Ticket/src/main/java/com/grupo01/spring/controller/TicketController.java
+++ Ticket/src/main/java/com/grupo01/spring/controller/TicketController.java
@@ -2,6 +2,8 @@ package com.grupo01.spring.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +18,8 @@ import com.grupo01.spring.service.TicketService;
 @RestController
 @RequestMapping("/ticket")
 public class TicketController {
+	
+	private static Logger log = LoggerFactory.getLogger(TicketController.class);
 
 	@Autowired
 	private TicketService ticketService;
@@ -23,6 +27,7 @@ public class TicketController {
 
 	@GetMapping("")
 	public List<TicketDTO> listTicket() {
+		log.info("------------ESTOY EN LIST TICKET CONTROLLER------");
 		return TicketDTO.of(ticketService.listTicket());
 	}
 
@@ -37,6 +42,7 @@ public class TicketController {
 	@PostMapping("{ticket_id}/add")
 	public void addEvent(@PathVariable int ticket_id, @RequestParam int event_id,
 			@RequestParam(defaultValue = "1") int cant) {
+		log.info("------------ESTOY EN ADD EVENT A TICKET CONTROLLER------");
 		ticketService.addEvent(ticket_id, event_id, cant);
 	}
 
